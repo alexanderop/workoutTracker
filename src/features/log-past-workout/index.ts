d399fdc6532a8de74c7934baa1dc8f3a333c732d
Alexander Opalic <opalic@gmx.net>
2025-12-15 19:28:43 +0100

    chore(log-past-workout): remove unused barrel file
    Remove index.ts barrel file that was created but never imported from.
    This fixes the knip "unused files" CI check failure.

--- src/features/log-past-workout/index.ts
+++
@@ -1,9 +0,0 @@
-// Components
-export { default as SourceSelector } from './components/SourceSelector.vue'
-export { default as DateDurationPicker } from './components/DateDurationPicker.vue'
-export { default as WorkoutBuilder } from './components/WorkoutBuilder.vue'
-export { default as StrengthBlockGrid } from './components/StrengthBlockGrid.vue'
-
-// Composables
-export { usePastWorkout } from './composables/usePastWorkout'
-export { usePastWorkoutSave } from './composables/usePastWorkoutSave'
